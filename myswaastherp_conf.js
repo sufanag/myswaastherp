3e4e56c95a79563af04e1d3474e69c02343f1df9
innoticaltesters <innotical.test@gmail.com>
2017-05-22 12:17:28 +0530

    "Modified code"

--- myswaastherp_conf.js
+++ myswaastherp_conf.js
@@ -16,11 +16,15 @@ exports.config={
     },
     specs:["Specs/signup-success.spec.js"],
     useAllAngular2AppRoots:true,
-    capabilities: {
+   /*capabilities: {
     browserName: 'chrome',
     'chromeOptions': {
       args: ['--test-type']
     }
+},*/
+   capabilities: {
+    browserName: 'firefox',
+    marionette: true,
   },
   onPrepare:function(){
      //browser.manage().window().setSize(1280,1024);
